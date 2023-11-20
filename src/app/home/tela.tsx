2339a7e24f9447c767a2c393bf1a2c25901d8725
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-20 14:03:17 -0300

    recarregando tela

--- src/app/home/tela.tsx
+++ src/app/home/tela.tsx
@@ -1,65 +1,66 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { View, FlatList, Text, ScrollView, Button, TouchableOpacity } from "react-native";
+import {
+  View,
+  FlatList,
+  Text,
+  ScrollView,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import RenderizarItem from "../../components/card/card";
 import { styles } from "./styled";
 import { Box, VStack } from "native-base";
 import api from "../../service/integracao";
 
-const Home = ({navigation} ) => {
+const Home = ({ navigation }) => {
   const [dados, setDados] = useState([]);
   const [erroMensagem, setErroMensagem] = useState(null);
 
-  useEffect(() => {
-    async function pegarDados() {
-      try {
-        const response = await api.get("/formulario"
-        );
-        setDados(response.data);
-      } catch (error) {
-        console.error("Erro ao buscar dados:", error.message);
-      }
+  const buscarDados = async () => {
+    try {
+      const response = await api.get("/formulario");
+      setDados(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error.message);
     }
+  };
 
-    pegarDados();
+  useEffect(() => {
+    buscarDados();
   }, []);
 
-      const handleEditar = async (editedItem) => {
-        try {
-          // Limpar mensagens de erro anteriores
-            setErroMensagem(null);
-           
-          // Fazer a requisição PUT para o backend com os dados atualizados
-          await api.put(`/formulario/${editedItem.id}`, editedItem);
+  const handleEditar = async (editedItem) => {
+    try {
+      // Limpar mensagens de erro anteriores
+      setErroMensagem(null);
 
-          // Atualizar a lista com os dados atualizados
-          setDados((prevDados) =>
-            prevDados.map((item) =>
-              item.id === editedItem.id ? editedItem : item
-            )
-          );
-        } catch (error) {
-          setErroMensagem(
-            `Cpf ja esta cadastrado, Digite outro cpf!!!`
-          );
-            setTimeout(() => {
-              setErroMensagem(null);
-            }, 5000);
-        }
-      };
+      // Fazer a requisição PUT para o backend com os dados atualizados
+      await api.put(`/formulario/${editedItem.id}`, editedItem);
 
+      // Atualizar a lista com os dados atualizados
+      setDados((prevDados) =>
+        prevDados.map((item) => (item.id === editedItem.id ? editedItem : item))
+      );
+    } catch (error) {
+      setErroMensagem(`Cpf já cadastrado, digite outro cpf!!!`);
+      setTimeout(() => {
+        setErroMensagem(null);
+      }, 5000);
+    }
+  };
 
-    const handleDeletar = async (itemId) => {
-      try {
-        // Fazer a requisição DELETE para o backend
-        await api.delete(`/formulario/${itemId}`);
+  const handleDeletar = async (itemId) => {
+    try {
+      // Fazer a requisição DELETE para o backend
+      await api.delete(`/formulario/${itemId}`);
 
-        // Atualizar a lista removendo o item excluído
-        setDados((prevDados) => prevDados.filter((item) => item.id !== itemId));
-      } catch (error) {
-        console.error("Erro ao deletar item:", error.message);
-      }
-    };
+      // Atualizar a lista removendo o item excluído
+      setDados((prevDados) => prevDados.filter((item) => item.id !== itemId));
+    } catch (error) {
+      console.error("Erro ao deletar item:", error.message);
+    }
+  };
 
   return (
     <ScrollView>
@@ -75,6 +76,19 @@ const Home = ({navigation} ) => {
             onPress={() => navigation.navigate("cadastro")}>
             <Text style={{ color: "white" }}>Cadastrar</Text>
           </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#28a745",
+              padding: 10,
+              borderRadius: 5,
+              alignItems: "center",
+              marginTop: 10,
+            }}
+            onPress={buscarDados}>
+            <Text style={{ color: "white" }}>Recarregar</Text>
+          </TouchableOpacity>
+
           <Text style={styles.titulo}>Lista de usuários:</Text>
 
           {dados.map((item) => (
